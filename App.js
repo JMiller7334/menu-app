262842a2d413f31f717f706802a0f1425b667041
park-err <prkr0114@gmail.com>
2023-09-20 21:12:49 -0500

    fixed file name

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import MenuBuilderView from './views/MenuBuilderView';
+import MenuBuilderView from './views/menuBuilderView';
 
 export default function App() {
   return (
